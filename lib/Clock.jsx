13c1110a9e36b34ca50fbf2455f3ccfd13082ac0
rosenpin <mail@tomerrosenfeld.com>
2023-04-11 11:37:33 +0300

    removed calendar icon

--- lib/Clock.jsx
+++ lib/Clock.jsx
@@ -7,7 +7,7 @@ const render = () => {
     const dateFormatOptions = { weekday: 'short', month: 'short', day: 'numeric' };
     const timeFormatOptions = { hour: 'numeric', minute: 'numeric', hour12: false };
 
-    const dateStr = symbols.calendar + " " + date.toLocaleDateString("en-SG", dateFormatOptions);
+    const dateStr = " " + date.toLocaleDateString("en-SG", dateFormatOptions);
     const timeStr = "    " + date.toLocaleTimeString("en-SG", timeFormatOptions);
 
     return (<div>
